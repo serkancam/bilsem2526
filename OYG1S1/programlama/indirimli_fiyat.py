e53f1538e09bced4b46cd78538659ccc183df138
serkan çam <serkancam22@gmail.com>
2025-10-18 10:47:08 +0300

    gosterim

--- OYG1S1/programlama/indirimli_fiyat.py
+++ OYG1S1/programlama/indirimli_fiyat.py
@@ -1,33 +1,25 @@
 # Bir kafede satılan ürünlerin fiyatları şöyledir: Çay (5 TL), Kahve (10 TL), Tatlı (15 TL). Kullanıcıdan kaç adet çay, kahve ve tatlı aldığını sorun. Toplam tutarı hesaplayın ve aşağıdaki indirim kurallarını uygulayarak son ödemesi gereken tutarı ekrana yazdırın:
-
 #     Toplam tutar 50 TL veya daha fazlaysa, %10 indirim yapın.
-
 #     Toplam tutar 100 TL veya daha fazlaysa, %20 indirim yapın.
-
 #     Diğer durumlarda indirim uygulamayın.
 
 
+cay_fiyati=5
+kahve_fiyati=10
+tatli_fiyati=15
+
+cay_adedi=int(input("cay adedi:"))
+kahve_adedi=int(input("kahve adedi:"))
+tatli_adedi=int(input("tatli adadi:"))
+
+fiyat=(cay_adedi*cay_fiyati)+(kahve_adedi*kahve_fiyati)+(tatli_adedi*tatli_fiyati)
 
 
+if fiyat>=50 and fiyat<100:
+    fiyat=fiyat*0.9
+elif fiyat>=100:
+    fiyat=fiyat*0.8
 
-
-
-cay_adet=int(input("kaç çay içtiniz:"))# 00001000  int("8")-->8, float("8")-->8.0
-kahve_adet=int(input("kaç kahve içtiniz:"))
-tatli_adet=int(input("kaç tatlı yediniz:"))
-
-cay_fiyat=5
-kahve_fiyat=10
-tatli_fiyat=15
-
-odenecek= (cay_adet*cay_fiyat)+(kahve_adet*kahve_fiyat)+(tatli_adet*tatli_fiyat)
-
-if odenecek>=100:#odenecek>=50 and odenecek<100
-    odenecek=odenecek*.8
-elif odenecek>=50:
-    odenecek=odenecek*.9
-
-print(f"{odenecek}")
+print(f"ödenecek tutar:{fiyat}")
     
-
-
+    
